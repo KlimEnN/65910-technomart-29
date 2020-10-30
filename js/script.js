6fb9319f79f4db347de3f88072b757928538619f
Andrey Klimenko <45719199+KlimEnN@users.noreply.github.com>
2020-10-30 20:44:03 +0200

    Исправил слайдер

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-const slides = document.querySelectorAll(".slider-item");
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll(".slider-item");
+const dots = slider.querySelectorAll(".slider-dot");
+const sliderButtons = slider.querySelectorAll(".slider-btn");
 let currentSlide = 0;
-const slideInterval = setInterval(() => {
-  slides[currentSlide].classList.remove("active");
+const activeClassName = 'active';
+
+sliderButtons.forEach((button) => {
+  button.addEventListener('click', sliderMove);
+})
+
+function sliderMove(evt) {
+  evt.preventDefault();
+  slides[currentSlide].classList.remove(activeClassName);
+  dots[currentSlide].classList.remove(activeClassName);
   currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add("active");
-}, 5000);
+  slides[currentSlide].classList.add(activeClassName);
+  dots[currentSlide].classList.add(activeClassName);
+};
